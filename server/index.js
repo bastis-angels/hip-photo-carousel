2e34f3ffe2ce53a1697371d9980bb7eff0d28456
Megan Keesee <hello@megankeesee.com>
2019-03-29 11:58:13 -0700

    update the server

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
 const express = require('express');
 let app = express();
+const port = 3000;
 
 const bodyParser = require('body-parser');
-// const db = require('../database/index.js');
+const Listings = require('../database/Listing.js')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 
 app.get('/listing/:listingID', (req, res) => {
-  //TODO
+  //TODO - get the camp listing your searching for
+  Listings.findOne()
 });
 
 app.patch('/listing/:listingID/:photoID', (req, res) => {
-  //TODO
+  //TODO - get the right listing image and increment the helpful vote by 1
 })
-
-const port = 3000;
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
