26a8445292587cd3eca9d0a387ce50ed2112ce82
Megan Keesee <hello@megankeesee.com>
2019-04-02 14:00:03 -0700

    Update seed data based on PR feedback

--- server/database/seed.js
+++ server/database/seed.js
@@ -7,25 +7,12 @@ const sampleListings = () => {
   const listings = [];
   
   for (let i = 1; i <= 100; i++) {
-    //listing data
-    // let id = i;
-    // let elevation = faker.random.number({min: 1000, max: 3000});
-    // let weather = faker.random.number({min: 50, max: 95});
-    // let distance = faker.random.number({min: 1, max: 10});
     let images = [];
   
     for (let j = 1; j <= 30; j++) {
-
-      //image data
-      // let imageId = j;
-      // let userID = faker.random.number({min: 1, max: 100});
-      // let imageURL = faker.image.imageUrl();
-      // let datePosted = faker.date.past();
-      // let location = faker.random.words(4);
   
       //images file
       images.push({
-        // '_id' : j,
         'userId' : faker.random.number({min: 1, max: 100}),
         'imageURL' : faker.image.imageUrl(),
         'datePosted' : faker.date.past(),
