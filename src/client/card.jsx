b0d2d1fb37ae2001f8ec5c92f370ad7052a063fc
Megan Keesee <hello@megankeesee.com>
2019-04-03 19:12:00 -0700

    format and style overlay components

--- src/client/card.jsx
+++ src/client/card.jsx
@@ -4,10 +4,23 @@ import React from 'react';
 const Card = (props) => {
   return (
     <div id={`card-${props.image}`} className="card">
-      <div className="userAvatar">
-        <i class="fas fa-campground"></i>
+
+      <div class="counter">1 / 30</div>
+
+      <div className="userInfo">
+        <div className="userAvatar">
+          <i class="fas fa-campground"></i>
+        </div>
+        <div className="userName">Eliana L.</div>
+        <div className="datePosted">Posted on <span>June 10, 2018</span></div>
       </div>
-      <button className="helpful"><span className="thumb"><i class="far fa-thumbs-up"></i></span> Helpful <span>5</span></button>
+
+      <button className="helpful"><span className="thumb"><i class="far fa-thumbs-up"></i> &nbsp; </span> Helpful <span> &nbsp; 5 </span></button>
+     
+      <div className="location">
+          <span><i class="fas fa-map-marker-alt"></i></span> <span>#1 PARK N SURF OCEANVIEW, WildTender Ranch</span>
+      </div>
+
       <div className="socialMediaIcons">
         <i className="fab fa-pinterest icon"></i>
         <i className="fab fa-facebook icon"></i>
